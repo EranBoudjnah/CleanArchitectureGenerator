6ccbfaa459f752386e2b2df05331e8c926e82814
Eran Boudjnah <eranbou@gmail.com>
2025-09-03 13:12:46 +0100

    Updated UseCase content and added ContinuousExecutingUseCase, UseCaseExecutor, DomainException and UnknownDomainException.

--- core/src/main/kotlin/com/mitteloupe/cag/core/generation/ArchitectureModulesContentGenerator.kt
+++ core/src/main/kotlin/com/mitteloupe/cag/core/generation/ArchitectureModulesContentGenerator.kt
@@ -90,43 +90,47 @@ class ArchitectureModulesContentGenerator {
 
     private fun generateDomainContent(
         architectureRoot: File,
-        projectNamespace: String,
+        moduleNamespace: String,
         architecturePackageName: String
     ) {
         val domainRoot = File(architectureRoot, "domain/src/main/java")
         val packageDirectory = buildPackageDirectory(domainRoot, architecturePackageName.toSegments())
 
-        generateUseCaseBase(packageDirectory, projectNamespace)
-        generateRepositoryBase(packageDirectory, projectNamespace)
+        generateUseCase(packageDirectory, moduleNamespace)
+        generateContinuousExecutingUseCase(packageDirectory, moduleNamespace)
+        generateRepositoryBase(packageDirectory, moduleNamespace)
+        generateDomainException(packageDirectory, moduleNamespace)
+        generateUnknownDomainException(packageDirectory, moduleNamespace)
+        generateUseCaseExecutor(packageDirectory, moduleNamespace)
     }
 
     private fun generatePresentationContent(
         architectureRoot: File,
-        projectNamespace: String,
+        moduleNamespace: String,
         architecturePackageName: String
     ) {
         val presentationRoot = File(architectureRoot, "presentation/src/main/java")
         val packageDirectory = buildPackageDirectory(presentationRoot, "$architecturePackageName.presentation".toSegments())
 
-        generateViewModelBase(packageDirectory, projectNamespace)
-        generateNavigationEventBase(packageDirectory, projectNamespace)
+        generateViewModelBase(packageDirectory, moduleNamespace)
+        generateNavigationEventBase(packageDirectory, moduleNamespace)
     }
 
     private fun generateUiContent(
         architectureRoot: File,
-        projectNamespace: String,
+        moduleNamespace: String,
         architecturePackageName: String
     ) {
         val uiRoot = File(architectureRoot, "ui/src/main/java")
         val packageDirectory = buildPackageDirectory(uiRoot, architecturePackageName.toSegments())
 
-        generateScreenBase(packageDirectory, projectNamespace)
-        generateMapperBase(packageDirectory, projectNamespace)
+        generateScreenBase(packageDirectory, moduleNamespace)
+        generateMapperBase(packageDirectory, moduleNamespace)
     }
 
-    private fun generateUseCaseBase(
+    private fun generateUseCase(
         packageDirectory: File,
-        projectNamespace: String
+        moduleNamespace: String
     ) {
         val useCaseFile = File(packageDirectory, "usecase/UseCase.kt")
         if (useCaseFile.exists()) {
@@ -135,14 +139,10 @@ class ArchitectureModulesContentGenerator {
 
         val content =
             """
-            package $projectNamespace.domain.usecase
+            package $moduleNamespace.domain.usecase
 
-            abstract class UseCase<in Input, Output> {
-                suspend operator fun invoke(input: Input): Output
-            }
-
-            abstract class NoInputUseCase<Output> {
-                suspend operator fun invoke(): Output
+            interface UseCase<REQUEST, RESULT> {
+                fun execute(input: REQUEST, onResult: (RESULT) -> Unit)
             }
             """.trimIndent()
 
@@ -154,9 +154,63 @@ class ArchitectureModulesContentGenerator {
         }
     }
 
+    private fun generateContinuousExecutingUseCase(
+        packageDirectory: File,
+        moduleNamespace: String
+    ) {
+        val useCaseFile = File(packageDirectory, "usecase/ContinuousExecutingUseCase.kt")
+        if (useCaseFile.exists()) {
+            return
+        }
+
+        val imports =
+            """
+import $moduleNamespace.coroutine.CoroutineContextProvider
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+""".optimizeImports()
+
+        val content =
+            """package $moduleNamespace.domain.usecase
+
+$imports
+abstract class ContinuousExecutingUseCase<REQUEST, RESULT>(
+    private val coroutineContextProvider: CoroutineContextProvider,
+    private val coroutineScope: CoroutineScope = CoroutineScope(Dispatchers.Main)
+) : UseCase<REQUEST, RESULT> {
+    final override fun execute(input: REQUEST, onResult: (RESULT) -> Unit) {
+        try {
+            coroutineScope.launch {
+                withContext(coroutineContextProvider.io) {
+                    executeInBackground(input).collect { result ->
+                        withContext(coroutineContextProvider.main) {
+                            onResult(result)
+                        }
+                    }
+                }
+            }
+        } catch (_: CancellationException) {
+        }
+    }
+
+    abstract fun executeInBackground(request: REQUEST): Flow<RESULT>
+}"""
+
+        runCatching {
+            useCaseFile.parentFile.mkdirs()
+            useCaseFile.writeText(content)
+        }.onFailure {
+            throw GenerationException("Failed to generate continuous executing use case: ${it.message}")
+        }
+    }
+
     private fun generateRepositoryBase(
         packageDirectory: File,
-        projectNamespace: String
+        moduleNamespace: String
     ) {
         val repositoryFile = File(packageDirectory, "repository/Repository.kt")
         if (repositoryFile.exists()) {
@@ -165,7 +219,7 @@ class ArchitectureModulesContentGenerator {
 
         val content =
             """
-            package $projectNamespace.domain.repository
+            package $moduleNamespace.domain.repository
 
             interface Repository
             """.trimIndent()
@@ -175,6 +229,105 @@ class ArchitectureModulesContentGenerator {
             repositoryFile.writeText(content)
         }.onFailure {
             throw GenerationException("Failed to generate repository: ${it.message}")
+        }
+    }
+
+    private fun generateUseCaseExecutor(
+        packageDirectory: File,
+        moduleNamespace: String
+    ) {
+        val useCaseExecutorFile = File(packageDirectory, "UseCaseExecutor.kt")
+        if (useCaseExecutorFile.exists()) {
+            return
+        }
+
+        val content =
+            """package $moduleNamespace.domain
+
+${
+                """
+import $moduleNamespace.domain.exception.DomainException
+import $moduleNamespace.domain.exception.UnknownDomainException
+import $moduleNamespace.domain.usecase.UseCase
+""".optimizeImports()
+            }
+class UseCaseExecutor {
+    fun <OUTPUT> execute(
+        useCase: UseCase<Unit, OUTPUT>,
+        onResult: (OUTPUT) -> Unit = {},
+        onException: (DomainException) -> Unit = {}
+    ) = execute(useCase, Unit, onResult, onException)
+
+    fun <INPUT, OUTPUT> execute(
+        useCase: UseCase<INPUT, OUTPUT>,
+        value: INPUT,
+        onResult: (OUTPUT) -> Unit = {},
+        onException: (DomainException) -> Unit = {}
+    ) {
+        try {
+            useCase.execute(value, onResult)
+        } catch (@Suppress("TooGenericExceptionCaught") throwable: Throwable) {
+            val domainException =
+                ((throwable as? DomainException) ?: UnknownDomainException(throwable))
+            onException(domainException)
+        }
+    }
+}
+"""
+
+        runCatching {
+            useCaseExecutorFile.parentFile.mkdirs()
+            useCaseExecutorFile.writeText(content)
+        }.onFailure {
+            throw GenerationException("Failed to generate use case executor: ${it.message}")
+        }
+    }
+
+    private fun generateDomainException(
+        packageDirectory: File,
+        moduleNamespace: String
+    ) {
+        val domainExceptionFile = File(packageDirectory, "exception/DomainException.kt")
+        if (domainExceptionFile.exists()) {
+            return
+        }
+
+        val content =
+            """
+            package $moduleNamespace.domain.exception
+
+            abstract class DomainException(cause: Throwable? = null) : Exception(cause)
+            """.trimIndent()
+
+        runCatching {
+            domainExceptionFile.parentFile.mkdirs()
+            domainExceptionFile.writeText(content)
+        }.onFailure {
+            throw GenerationException("Failed to generate domain exception: ${it.message}")
+        }
+    }
+
+    private fun generateUnknownDomainException(
+        packageDirectory: File,
+        moduleNamespace: String
+    ) {
+        val domainExceptionFile = File(packageDirectory, "exception/UnknownDomainException.kt")
+        if (domainExceptionFile.exists()) {
+            return
+        }
+
+        val content =
+            """
+            package $moduleNamespace.domain.exception
+
+            class UnknownDomainException(cause: Throwable? = null) : DomainException(cause)
+            """.trimIndent()
+
+        runCatching {
+            domainExceptionFile.parentFile.mkdirs()
+            domainExceptionFile.writeText(content)
+        }.onFailure {
+            throw GenerationException("Failed to generate unknown domain exception: ${it.message}")
         }
     }
 
@@ -188,65 +341,66 @@ class ArchitectureModulesContentGenerator {
         }
 
         val content =
-            """
-            package $architecturePackage.presentation.viewmodel
+            """package $architecturePackage.presentation.viewmodel
 
-            import $architecturePackage.domain.UseCaseExecutor
-            import $architecturePackage.domain.exception.DomainException
-            import $architecturePackage.domain.usecase.UseCase
-            import $architecturePackage.presentation.navigation.PresentationNavigationEvent
-            import $architecturePackage.presentation.notification.PresentationNotification
-            import kotlinx.coroutines.MainScope
-            import kotlinx.coroutines.flow.Flow
-            import kotlinx.coroutines.flow.MutableSharedFlow
-            import kotlinx.coroutines.launch
-
-            abstract class BaseViewModel<VIEW_STATE : Any, NOTIFICATION : PresentationNotification>(
-                private val useCaseExecutor: UseCaseExecutor
-            ) {
-                val viewState: Flow<VIEW_STATE>
-                    field = MutableSharedFlow()
-
-                val notification: Flow<NOTIFICATION>
-                    field = MutableSharedFlow()
-
-                val navigationEvent: Flow<PresentationNavigationEvent>
-                    field = MutableSharedFlow()
-            
-                protected fun updateViewState(newState: VIEW_STATE) {
-                    MainScope().launch {
-                        viewState.emit(newState)
-                    }
-                }
-
-                protected fun notify(notification: NOTIFICATION) {
-                    MainScope().launch {
-                        this@BaseViewModel.notification.emit(notification)
-                    }
-                }
-
-                protected fun emitNavigationEvent(navigationEvent: PresentationNavigationEvent) {
-                    MainScope().launch {
-                        this@BaseViewModel.navigationEvent.emit(navigationEvent)
-                    }
-                }
-
-                protected operator fun <OUTPUT> UseCase<Unit, OUTPUT>.invoke(
-                    onResult: (OUTPUT) -> Unit = {},
-                    onException: (DomainException) -> Unit = {}
-                ) {
-                    useCaseExecutor.execute(this, onResult, onException)
-                }
-
-                protected operator fun <INPUT, OUTPUT> UseCase<INPUT, OUTPUT>.invoke(
-                    value: INPUT,
-                    onResult: (OUTPUT) -> Unit = {},
-                    onException: (DomainException) -> Unit = {}
-                ) {
-                    useCaseExecutor.execute(this, value, onResult, onException)
-                }
+${
+                """
+import $architecturePackage.domain.UseCaseExecutor
+import $architecturePackage.domain.exception.DomainException
+import $architecturePackage.domain.usecase.UseCase
+import $architecturePackage.presentation.navigation.PresentationNavigationEvent
+import $architecturePackage.presentation.notification.PresentationNotification
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.launch
+""".optimizeImports()
             }
-            """.trimIndent()
+abstract class BaseViewModel<VIEW_STATE : Any, NOTIFICATION : PresentationNotification>(
+    private val useCaseExecutor: UseCaseExecutor
+) {
+    val viewState: Flow<VIEW_STATE>
+        field = MutableSharedFlow()
+
+    val notification: Flow<NOTIFICATION>
+        field = MutableSharedFlow()
+
+    val navigationEvent: Flow<PresentationNavigationEvent>
+        field = MutableSharedFlow()
+
+    protected fun updateViewState(newState: VIEW_STATE) {
+        MainScope().launch {
+            viewState.emit(newState)
+        }
+    }
+
+    protected fun notify(notification: NOTIFICATION) {
+        MainScope().launch {
+            this@BaseViewModel.notification.emit(notification)
+        }
+    }
+
+    protected fun emitNavigationEvent(navigationEvent: PresentationNavigationEvent) {
+        MainScope().launch {
+            this@BaseViewModel.navigationEvent.emit(navigationEvent)
+        }
+    }
+
+    protected operator fun <OUTPUT> UseCase<Unit, OUTPUT>.invoke(
+        onResult: (OUTPUT) -> Unit = {},
+        onException: (DomainException) -> Unit = {}
+    ) {
+        useCaseExecutor.execute(this, onResult, onException)
+    }
+
+    protected operator fun <INPUT, OUTPUT> UseCase<INPUT, OUTPUT>.invoke(
+        value: INPUT,
+        onResult: (OUTPUT) -> Unit = {},
+        onException: (DomainException) -> Unit = {}
+    ) {
+        useCaseExecutor.execute(this, value, onResult, onException)
+    }
+}"""
 
         runCatching {
             viewModelFile.parentFile.mkdirs()
@@ -258,7 +412,7 @@ class ArchitectureModulesContentGenerator {
 
     private fun generateNavigationEventBase(
         packageDirectory: File,
-        projectNamespace: String
+        moduleNamespace: String
     ) {
         val navigationFile = File(packageDirectory, "navigation/PresentationNavigationEvent.kt")
         if (navigationFile.exists()) {
@@ -267,7 +421,7 @@ class ArchitectureModulesContentGenerator {
 
         val content =
             """
-            package $projectNamespace.presentation.navigation
+            package $moduleNamespace.presentation.navigation
 
             sealed class PresentationNavigationEvent
             """.trimIndent()
@@ -282,7 +436,7 @@ class ArchitectureModulesContentGenerator {
 
     private fun generateScreenBase(
         packageDirectory: File,
-        projectNamespace: String
+        moduleNamespace: String
     ) {
         val screenFile = File(packageDirectory, "view/Screen.kt")
         if (screenFile.exists()) {
@@ -290,16 +444,17 @@ class ArchitectureModulesContentGenerator {
         }
 
         val content =
-            """
-            package $projectNamespace.ui.view
+            """package $moduleNamespace.ui.view
 
-            import androidx.compose.runtime.Composable
-
-            interface Screen {
-                @Composable
-                fun Content()
+${
+                """
+import androidx.compose.runtime.Composable
+""".optimizeImports()
             }
-            """.trimIndent()
+interface Screen {
+    @Composable
+    fun Content()
+}"""
 
         runCatching {
             screenFile.parentFile.mkdirs()
@@ -311,7 +466,7 @@ class ArchitectureModulesContentGenerator {
 
     private fun generateMapperBase(
         packageDirectory: File,
-        projectNamespace: String
+        moduleNamespace: String
     ) {
         val mapperFile = File(packageDirectory, "mapper/Mapper.kt")
         if (mapperFile.exists()) {
@@ -320,7 +475,7 @@ class ArchitectureModulesContentGenerator {
 
         val content =
             """
-            package $projectNamespace.ui.mapper
+            package $moduleNamespace.ui.mapper
 
             interface Mapper<Input, Output> {
                 fun map(input: Input): Output
