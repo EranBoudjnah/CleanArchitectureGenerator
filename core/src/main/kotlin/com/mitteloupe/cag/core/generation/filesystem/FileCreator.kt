714ba2e72856acc07b516b533699b24214b1c74d
Eran Boudjnah <eranbou@gmail.com>
2025-09-05 15:38:05 +0100

    Added FileCreator.

---
+++ core/src/main/kotlin/com/mitteloupe/cag/core/generation/filesystem/FileCreator.kt
@@ -0,0 +1,28 @@
+package com.mitteloupe.cag.core.generation.filesystem
+
+import com.mitteloupe.cag.core.GenerationException
+import java.io.File
+
+object FileCreator {
+    fun createDirectoryIfNotExists(directory: File) {
+        if (!directory.exists()) {
+            runCatching { directory.mkdirs() }.getOrElse { GenerationException("Failed to create directory: ${directory.absolutePath}") }
+        }
+    }
+
+    fun createFileIfNotExists(
+        file: File,
+        contentProvider: () -> String
+    ) {
+        if (file.exists()) {
+            return
+        }
+
+        val content = contentProvider()
+        runCatching { file.writeText(content) }
+            .onFailure {
+                val absolutePath = file.absolutePath
+                throw GenerationException("Failed to create file: $absolutePath: ${it.message}")
+            }
+    }
+}
