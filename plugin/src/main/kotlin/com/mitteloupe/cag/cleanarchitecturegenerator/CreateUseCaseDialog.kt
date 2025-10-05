ffab2d165a479a0a8001bfb89ea42989925eabd7
Eran Boudjnah <eranbou@gmail.com>
2025-10-05 15:07:21 +0100

    Further migration to UI DSL v2.

--- plugin/src/main/kotlin/com/mitteloupe/cag/cleanarchitecturegenerator/CreateUseCaseDialog.kt
+++ plugin/src/main/kotlin/com/mitteloupe/cag/cleanarchitecturegenerator/CreateUseCaseDialog.kt
@@ -1,28 +1,26 @@
 package com.mitteloupe.cag.cleanarchitecturegenerator
 
 import com.intellij.icons.AllIcons
-import com.intellij.openapi.fileChooser.FileChooser
 import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.ComboBox
+import com.intellij.openapi.ui.DialogPanel
 import com.intellij.openapi.ui.DialogWrapper
-import com.intellij.openapi.ui.TextFieldWithBrowseButton
 import com.intellij.openapi.ui.ValidationInfo
-import com.intellij.openapi.vfs.LocalFileSystem
-import com.intellij.ui.components.JBLabel
-import com.intellij.ui.components.JBTextField
-import com.intellij.ui.dsl.builder.Align
 import com.intellij.ui.dsl.builder.bindText
 import com.intellij.ui.dsl.builder.panel
 import com.intellij.ui.dsl.builder.whenItemChangedFromUi
 import com.intellij.util.ui.UIUtil
-import com.mitteloupe.cag.cleanarchitecturegenerator.form.OnChangeDocumentListener
 import com.mitteloupe.cag.cleanarchitecturegenerator.form.PredicateDocumentFilter
 import com.mitteloupe.cag.cleanarchitecturegenerator.validation.SymbolValidator
 import java.awt.EventQueue.invokeLater
 import java.io.File
 import javax.swing.DefaultComboBoxModel
+import javax.swing.JComponent
+import javax.swing.JLabel
 import javax.swing.JTextField
+import javax.swing.event.DocumentEvent
+import javax.swing.event.DocumentListener
 import javax.swing.text.AbstractDocument
 
 private const val USE_CASE_SUFFIX = "UseCase"
@@ -30,15 +28,17 @@ private const val DEFAULT_USE_CASE_NAME = "DoSomething"
 private const val DEFAULT_DATA_TYPE = "Unit"
 
 class CreateUseCaseDialog(
-    project: Project?,
+    private val project: Project?,
     suggestedDirectory: File?
 ) : DialogWrapper(project) {
-    private val useCaseNameTextField = JBTextField()
-    private val directoryField = TextFieldWithBrowseButton()
+    private val initialDirectory = suggestedDirectory?.absolutePath.orEmpty()
+    private lateinit var useCaseNameTextField: JComponent
+    private var useCaseNameText: String = ""
+    private var directoryPath: String = initialDirectory
     private lateinit var inputDataTypeComboBox: ComboBox<String>
-    private val inputWarningLabel = JBLabel()
+    private lateinit var inputWarningLabel: JLabel
     private lateinit var outputDataTypeComboBox: ComboBox<String>
-    private val outputWarningLabel = JBLabel()
+    private lateinit var outputWarningLabel: JLabel
     private val modelClassFinder = ModelClassFinder()
     private val symbolValidator = SymbolValidator()
     private val inputDataTypeModel = DefaultComboBoxModel<String>()
@@ -48,7 +48,7 @@ class CreateUseCaseDialog(
         get() = useCaseName.removeSuffix(USE_CASE_SUFFIX) + USE_CASE_SUFFIX
 
     private val useCaseName: String
-        get() = useCaseNameTextField.text.trim()
+        get() = useCaseNameText.trim()
 
     private var _inputDataType: String? = null
     val inputDataType: String?
@@ -59,92 +59,91 @@ class CreateUseCaseDialog(
         get() = _outputDataType
 
     val destinationDirectory: File?
-        get() = directoryField.text.trim().takeIf { it.isNotEmpty() }?.let { File(it) }
+        get() = directoryPath.trim().takeIf { it.isNotEmpty() }?.let { File(it) }
 
     init {
         title = CleanArchitectureGeneratorBundle.message("info.usecase.generator.title")
         init()
 
-        useCaseNameTextField.columns = 20
-        directoryField.text = suggestedDirectory?.absolutePath.orEmpty()
-        directoryField.addActionListener {
-            val descriptor = FileChooserDescriptorFactory.createSingleFolderDescriptor()
-            val initialDirectory = LocalFileSystem.getInstance().findFileByIoFile(File(directoryField.text))
-            val chosen = FileChooser.chooseFile(descriptor, project, initialDirectory)
-            if (chosen != null) {
-                directoryField.text = chosen.path
-                setupDataTypeComboBoxes()
-            }
-        }
-
-        useCaseNameTextField.text = DEFAULT_USE_CASE_NAME
-        useCaseNameTextField.selectAll()
-
-        (useCaseNameTextField.document as AbstractDocument).documentFilter =
-            PredicateDocumentFilter { !it.isWhitespace() }
-
         setupDataTypeComboBoxes()
         setupWarningLabels()
     }
 
-    override fun getPreferredFocusedComponent() = useCaseNameTextField
-
-    override fun show() {
-        super.show()
-
-        invokeLater {
-            validateFieldOnChange()
-        }
-    }
-
-    override fun createCenterPanel() =
-        panel {
+    override fun createCenterPanel(): DialogPanel {
+        return panel {
             row(CleanArchitectureGeneratorBundle.message("dialog.usecase.name.label")) {
                 textField()
-                    .bindText(useCaseNameTextField::getText, useCaseNameTextField::setText)
-                cell(
-                    JBLabel(USE_CASE_SUFFIX).apply {
-                        foreground = UIUtil.getLabelDisabledForeground()
+                    .bindText(::useCaseNameText)
+                    .onChanged { useCaseNameText = it.text }
+                    .applyToComponent {
+                        useCaseNameTextField = this
+                        invokeLater {
+                            columns = 20
+                            text = DEFAULT_USE_CASE_NAME
+                            selectAll()
+                            requestFocusInWindow()
+                        }
+                        (document as AbstractDocument).documentFilter = PredicateDocumentFilter { !it.isWhitespace() }
                     }
-                )
+                label(USE_CASE_SUFFIX)
+                    .applyToComponent { foreground = UIUtil.getLabelDisabledForeground() }
             }
             row(CleanArchitectureGeneratorBundle.message("dialog.usecase.input.type.label")) {
                 @Suppress("UnstableApiUsage")
                 comboBox(inputDataTypeModel)
                     .whenItemChangedFromUi { _inputDataType = it }
                     .applyToComponent {
-                        isEditable = true
-                        (editor.editorComponent as JTextField).validateOnChange {
-                            _inputDataType = text
-                        }
-                        selectedItem = DEFAULT_DATA_TYPE
                         inputDataTypeComboBox = this
+                        isEditable = true
+                        val textField = editor.editorComponent as JTextField
+                        textField.handleChange {
+                            println("ERAN: CHANGE! ${textField.text}")
+                            _inputDataType = textField.text
+                            inputDataType.validateDataType(inputWarningLabel)
+                        }
+                        invokeLater { selectedItem = DEFAULT_DATA_TYPE }
                     }
-                cell(inputWarningLabel)
+                label("").applyToComponent { inputWarningLabel = this }
             }
             row(CleanArchitectureGeneratorBundle.message("dialog.usecase.output.type.label")) {
                 @Suppress("UnstableApiUsage")
                 comboBox(outputDataTypeModel)
                     .whenItemChangedFromUi { _outputDataType = it }
                     .applyToComponent {
-                        isEditable = true
-                        (editor.editorComponent as JTextField).validateOnChange {
-                            _inputDataType = text
-                        }
-                        addActionListener {
-                            validateFieldOnChange()
-                        }
-                        selectedItem = DEFAULT_DATA_TYPE
                         outputDataTypeComboBox = this
+                        isEditable = true
+                        val textField = editor.editorComponent as JTextField
+                        textField.handleChange {
+                            println("ERAN: CHANGE! ${textField.text}")
+                            _outputDataType = textField.text
+                            outputDataType.validateDataType(outputWarningLabel)
+                        }
+                        invokeLater { selectedItem = DEFAULT_DATA_TYPE }
                     }
-                cell(outputWarningLabel)
+                label("").applyToComponent { outputWarningLabel = this }
             }
             row(CleanArchitectureGeneratorBundle.message("dialog.usecase.directory.field.label")) {
-                cell(directoryField)
-                    .comment(CleanArchitectureGeneratorBundle.message("dialog.usecase.directory.comment"))
-                    .align(Align.FILL)
+                @Suppress("UnstableApiUsage")
+                textFieldWithBrowseButton(
+                    project = project,
+                    fileChooserDescriptor = FileChooserDescriptorFactory.createSingleFolderDescriptor(),
+                    fileChosen = {
+                        it.path.also { path ->
+                            directoryPath = path
+                            setupDataTypeComboBoxes()
+                        }
+                    }
+                )
+                    .bindText(::directoryPath)
+                    .applyToComponent {
+                        invokeLater {
+                            text = initialDirectory
+                        }
+                        toolTipText = CleanArchitectureGeneratorBundle.message("dialog.usecase.directory.comment")
+                    }
             }
         }
+    }
 
     override fun doValidate(): ValidationInfo? =
         if (useCaseName.isEmpty()) {
@@ -159,11 +158,6 @@ class CreateUseCaseDialog(
     private fun setupWarningLabels() {
         inputWarningLabel.clearWarning()
         outputWarningLabel.clearWarning()
-    }
-
-    private fun validateFieldOnChange() {
-        inputDataType.validateDataType(inputWarningLabel)
-        outputDataType.validateDataType(outputWarningLabel)
     }
 
     private fun validateInputOutputTypes(): ValidationInfo? {
@@ -200,7 +194,7 @@ class CreateUseCaseDialog(
         return null
     }
 
-    private fun String?.validateDataType(warningLabel: JBLabel) {
+    private fun String?.validateDataType(warningLabel: JLabel) {
         if (isNullOrEmpty()) {
             warningLabel.clearWarning()
             return
@@ -213,26 +207,24 @@ class CreateUseCaseDialog(
 
         val destinationDirectory = destinationDirectory
         if (destinationDirectory != null && !symbolValidator.isValidSymbolInContext(this, destinationDirectory)) {
-            warningLabel.showFieldWarning(
-                CleanArchitectureGeneratorBundle.message("error.symbol.not.found")
-            )
+            warningLabel.showFieldWarning(CleanArchitectureGeneratorBundle.message("error.symbol.not.found"))
         } else {
             warningLabel.clearWarning()
         }
     }
 
-    private fun JBLabel.clearWarning() {
+    private fun JLabel.clearWarning() {
         icon = null
         text = " "
     }
 
-    private fun JBLabel.showFieldWarning(message: String) {
+    private fun JLabel.showFieldWarning(message: String) {
         icon = AllIcons.General.Warning
         text = message
     }
 
     private fun setupDataTypeComboBoxes() {
-        destinationDirectory?.let { destinationDirectory ->
+        File(initialDirectory).let { destinationDirectory ->
             val modelClasses = modelClassFinder.findModelClasses(destinationDirectory)
             val allOptions = ModelClassFinder.PRIMITIVE_TYPES + modelClasses
 
@@ -241,11 +233,14 @@ class CreateUseCaseDialog(
         }
     }
 
-    private fun JTextField.validateOnChange(onChange: JTextField.() -> Unit) {
+    private fun JTextField.handleChange(onChange: () -> Unit) {
         document.addDocumentListener(
-            OnChangeDocumentListener {
-                onChange()
-                validateFieldOnChange()
+            object : DocumentListener {
+                override fun insertUpdate(e: DocumentEvent) = onChange()
+
+                override fun removeUpdate(e: DocumentEvent) = onChange()
+
+                override fun changedUpdate(e: DocumentEvent) = onChange()
             }
         )
     }
--- plugin/src/main/kotlin/com/mitteloupe/cag/cleanarchitecturegenerator/CreateViewModelDialog.kt
+++ plugin/src/main/kotlin/com/mitteloupe/cag/cleanarchitecturegenerator/CreateViewModelDialog.kt
@@ -1,19 +1,15 @@
 package com.mitteloupe.cag.cleanarchitecturegenerator
 
-import com.intellij.openapi.fileChooser.FileChooser
 import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.DialogWrapper
-import com.intellij.openapi.ui.TextFieldWithBrowseButton
 import com.intellij.openapi.ui.ValidationInfo
-import com.intellij.openapi.vfs.LocalFileSystem
-import com.intellij.ui.components.JBTextField
-import com.intellij.ui.dsl.builder.Align
 import com.intellij.ui.dsl.builder.bindText
 import com.intellij.ui.dsl.builder.panel
 import com.intellij.util.ui.UIUtil
 import com.mitteloupe.cag.cleanarchitecturegenerator.form.PredicateDocumentFilter
 import com.mitteloupe.cag.cleanarchitecturegenerator.validation.SymbolValidator
+import java.awt.EventQueue.invokeLater
 import java.io.File
 import javax.swing.JComponent
 import javax.swing.text.AbstractDocument
@@ -22,57 +18,62 @@ private const val VIEW_MODEL_SUFFIX = "ViewModel"
 private const val DEFAULT_VIEW_MODEL_NAME = "My"
 
 class CreateViewModelDialog(
-    project: Project?,
-    suggestedDirectory: File?
+    private val project: Project?,
+    private val suggestedDirectory: File?
 ) : DialogWrapper(project) {
-    private val viewModelNameTextField = JBTextField()
-    private val directoryField = TextFieldWithBrowseButton()
+    private var viewModelNameText: String = ""
+    private var directoryPath: String = ""
     private val symbolValidator = SymbolValidator()
 
     val viewModelNameWithSuffix: String
         get() = viewModelName.removeSuffix(VIEW_MODEL_SUFFIX) + VIEW_MODEL_SUFFIX
 
     private val viewModelName: String
-        get() = viewModelNameTextField.text.trim()
+        get() = viewModelNameText.trim()
 
     val destinationDirectory: File?
-        get() = if (directoryField.text.isNotEmpty()) File(directoryField.text) else null
+        get() =
+            if (directoryPath.isNotEmpty()) {
+                File(directoryPath)
+            } else {
+                null
+            }
 
     init {
         title = CleanArchitectureGeneratorBundle.message("info.viewmodel.generator.title")
         init()
-
-        viewModelNameTextField.columns = 20
-        directoryField.text = suggestedDirectory?.absolutePath.orEmpty()
-        directoryField.addActionListener {
-            val descriptor = FileChooserDescriptorFactory.createSingleFolderDescriptor()
-            val initialDirectory = LocalFileSystem.getInstance().findFileByIoFile(File(directoryField.text))
-            val chosen = FileChooser.chooseFile(descriptor, project, initialDirectory)
-            if (chosen != null) {
-                directoryField.text = chosen.path
-            }
-        }
-
-        viewModelNameTextField.text = DEFAULT_VIEW_MODEL_NAME
-        viewModelNameTextField.selectAll()
-
-        (viewModelNameTextField.document as AbstractDocument).documentFilter =
-            PredicateDocumentFilter { !it.isWhitespace() }
     }
 
     override fun createCenterPanel(): JComponent =
         panel {
             row(CleanArchitectureGeneratorBundle.message("dialog.viewmodel.name.label")) {
                 textField()
-                    .bindText(viewModelNameTextField::getText, viewModelNameTextField::setText)
+                    .bindText(::viewModelNameText)
+                    .onChanged { viewModelNameText = it.text }
+                    .applyToComponent {
+                        invokeLater {
+                            columns = 20
+                            text = DEFAULT_VIEW_MODEL_NAME
+                            selectAll()
+                        }
+                        toolTipText = CleanArchitectureGeneratorBundle.message("dialog.viewmodel.directory.tooltip")
+                        (document as AbstractDocument).documentFilter = PredicateDocumentFilter { !it.isWhitespace() }
+                    }
                 label(VIEW_MODEL_SUFFIX)
                     .applyToComponent { foreground = UIUtil.getLabelDisabledForeground() }
             }
 
             row(CleanArchitectureGeneratorBundle.message("dialog.viewmodel.directory.field.label")) {
-                cell(directoryField)
-                    .comment(CleanArchitectureGeneratorBundle.message("dialog.viewmodel.directory.comment"))
-                    .align(Align.FILL)
+                @Suppress("UnstableApiUsage")
+                textFieldWithBrowseButton(
+                    project = project,
+                    fileChooserDescriptor = FileChooserDescriptorFactory.createSingleFolderDescriptor()
+                )
+                    .bindText(::directoryPath)
+                    .applyToComponent {
+                        invokeLater { text = suggestedDirectory?.absolutePath.orEmpty() }
+                        toolTipText = CleanArchitectureGeneratorBundle.message("dialog.viewmodel.directory.tooltip")
+                    }
             }
         }
 
