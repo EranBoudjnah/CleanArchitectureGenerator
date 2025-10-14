1b9f767f99d4ebceb9946bad953adbd438680255
Eran Boudjnah <eranbou@gmail.com>
2025-10-14 11:47:07 +0100

    Unified Generator instantiation.

--- cli/src/main/kotlin/com/mitteloupe/cag/cli/Main.kt
+++ cli/src/main/kotlin/com/mitteloupe/cag/cli/Main.kt
@@ -3,37 +3,12 @@ package com.mitteloupe.cag.cli
 import com.mitteloupe.cag.cli.HelpContent.USAGE_SYNTAX
 import com.mitteloupe.cag.cli.configuration.ClientConfigurationLoader
 import com.mitteloupe.cag.cli.filesystem.CliFileSystemBridge
-import com.mitteloupe.cag.core.DirectoryFinder
 import com.mitteloupe.cag.core.GenerationException
 import com.mitteloupe.cag.core.Generator
+import com.mitteloupe.cag.core.GeneratorFactory
 import com.mitteloupe.cag.core.NamespaceResolver
 import com.mitteloupe.cag.core.findGradleProjectRoot
-import com.mitteloupe.cag.core.generation.BuildSrcContentCreator
-import com.mitteloupe.cag.core.generation.ConfigurationFileCreator
-import com.mitteloupe.cag.core.generation.KotlinFileCreator
-import com.mitteloupe.cag.core.generation.SettingsFileUpdater
-import com.mitteloupe.cag.core.generation.app.AppModuleContentGenerator
-import com.mitteloupe.cag.core.generation.app.DataSourceDependencyInjectionModuleCreator
-import com.mitteloupe.cag.core.generation.architecture.ArchitectureModulesContentGenerator
-import com.mitteloupe.cag.core.generation.architecture.CoroutineModuleContentGenerator
-import com.mitteloupe.cag.core.generation.bulk.ArchitectureFilesGenerator
-import com.mitteloupe.cag.core.generation.bulk.DataSourceFilesGenerator
-import com.mitteloupe.cag.core.generation.bulk.DataSourceModulesGenerator
-import com.mitteloupe.cag.core.generation.bulk.FeatureFilesGenerator
-import com.mitteloupe.cag.core.generation.bulk.ProjectTemplateFilesGenerator
-import com.mitteloupe.cag.core.generation.bulk.ViewModelFilesGenerator
-import com.mitteloupe.cag.core.generation.filesystem.FileCreator
-import com.mitteloupe.cag.core.generation.gradle.GradleFileCreator
-import com.mitteloupe.cag.core.generation.gradle.GradlePropertiesFileCreator
-import com.mitteloupe.cag.core.generation.gradle.GradleWrapperCreator
-import com.mitteloupe.cag.core.generation.layer.DataLayerContentGenerator
-import com.mitteloupe.cag.core.generation.layer.DataSourceImplementationCreator
-import com.mitteloupe.cag.core.generation.layer.DataSourceInterfaceCreator
-import com.mitteloupe.cag.core.generation.layer.DomainLayerContentGenerator
-import com.mitteloupe.cag.core.generation.layer.PresentationLayerContentGenerator
-import com.mitteloupe.cag.core.generation.layer.UiLayerContentGenerator
 import com.mitteloupe.cag.core.generation.versioncatalog.VersionCatalogSettingsAccessor
-import com.mitteloupe.cag.core.generation.versioncatalog.VersionCatalogUpdater
 import com.mitteloupe.cag.core.request.GenerateArchitectureRequest
 import com.mitteloupe.cag.core.request.GenerateFeatureRequestBuilder
 import com.mitteloupe.cag.core.request.GenerateProjectTemplateRequest
@@ -356,79 +331,7 @@ private fun printHelpMessage(topic: String?) {
     }
 }
 
-private fun produceGenerator(): Generator {
-    val fileCreator = FileCreator(CliFileSystemBridge())
-    val directoryFinder = DirectoryFinder()
-    val kotlinFileCreator = KotlinFileCreator(fileCreator)
-    val gradleFileCreator = GradleFileCreator(fileCreator)
-    val gradleWrapperCreator = GradleWrapperCreator(fileCreator)
-    val buildSrcContentCreator = BuildSrcContentCreator(fileCreator)
-    val gradlePropertiesFileCreator = GradlePropertiesFileCreator(fileCreator)
-    val catalogUpdater = VersionCatalogUpdater(fileCreator)
-    val architectureModulesContentGenerator = ArchitectureModulesContentGenerator(gradleFileCreator, catalogUpdater)
-    val coroutineModuleContentGenerator = CoroutineModuleContentGenerator(gradleFileCreator, catalogUpdater)
-    val uiLayerContentGenerator = UiLayerContentGenerator(kotlinFileCreator)
-    val dataLayerContentGenerator = DataLayerContentGenerator(kotlinFileCreator)
-    val dataSourceDependencyInjectionModuleCreator = DataSourceDependencyInjectionModuleCreator(fileCreator)
-    val dataSourceInterfaceCreator = DataSourceInterfaceCreator(fileCreator)
-    val dataSourceImplementationCreator = DataSourceImplementationCreator(fileCreator)
-    val settingsFileUpdater = SettingsFileUpdater(fileCreator)
-    val domainLayerContentGenerator = DomainLayerContentGenerator(kotlinFileCreator)
-    val presentationLayerContentGenerator = PresentationLayerContentGenerator(kotlinFileCreator, fileCreator)
-    val configurationFileCreator = ConfigurationFileCreator(fileCreator)
-    val appModuleContentGenerator = AppModuleContentGenerator(fileCreator, directoryFinder)
-    val dataSourceModulesGenerator =
-        DataSourceModulesGenerator(
-            catalogUpdater,
-            gradleFileCreator,
-            settingsFileUpdater
-        )
-    val architectureFilesGenerator =
-        ArchitectureFilesGenerator(
-            coroutineModuleContentGenerator,
-            architectureModulesContentGenerator,
-            settingsFileUpdater,
-            buildSrcContentCreator,
-            configurationFileCreator
-        )
-    val featureFilesGenerator =
-        FeatureFilesGenerator(
-            catalogUpdater,
-            gradleFileCreator,
-            domainLayerContentGenerator,
-            presentationLayerContentGenerator,
-            dataLayerContentGenerator,
-            uiLayerContentGenerator,
-            settingsFileUpdater,
-            configurationFileCreator,
-            appModuleContentGenerator
-        )
-    return Generator(
-        domainLayerContentGenerator,
-        featureFilesGenerator,
-        dataSourceModulesGenerator,
-        DataSourceFilesGenerator(
-            dataSourceInterfaceCreator,
-            dataSourceImplementationCreator,
-            dataSourceDependencyInjectionModuleCreator
-        ),
-        architectureFilesGenerator,
-        ProjectTemplateFilesGenerator(
-            catalogUpdater,
-            settingsFileUpdater,
-            configurationFileCreator,
-            gradleFileCreator,
-            gradlePropertiesFileCreator,
-            gradleWrapperCreator,
-            appModuleContentGenerator,
-            buildSrcContentCreator,
-            dataSourceModulesGenerator,
-            architectureFilesGenerator,
-            featureFilesGenerator
-        ),
-        ViewModelFilesGenerator(presentationLayerContentGenerator)
-    )
-}
+private fun produceGenerator(): Generator = GeneratorFactory(CliFileSystemBridge()).create()
 
 private fun executeAndReport(operation: () -> Unit) =
     try {
--- core/src/test/kotlin/com/mitteloupe/cag/core/GeneratorTest.kt
+++ core/src/test/kotlin/com/mitteloupe/cag/core/GeneratorTest.kt
@@ -1,31 +1,6 @@
 package com.mitteloupe.cag.core
 
 import com.mitteloupe.cag.core.fake.FakeFileSystemBridge
-import com.mitteloupe.cag.core.generation.BuildSrcContentCreator
-import com.mitteloupe.cag.core.generation.ConfigurationFileCreator
-import com.mitteloupe.cag.core.generation.KotlinFileCreator
-import com.mitteloupe.cag.core.generation.SettingsFileUpdater
-import com.mitteloupe.cag.core.generation.app.AppModuleContentGenerator
-import com.mitteloupe.cag.core.generation.app.DataSourceDependencyInjectionModuleCreator
-import com.mitteloupe.cag.core.generation.architecture.ArchitectureModulesContentGenerator
-import com.mitteloupe.cag.core.generation.architecture.CoroutineModuleContentGenerator
-import com.mitteloupe.cag.core.generation.bulk.ArchitectureFilesGenerator
-import com.mitteloupe.cag.core.generation.bulk.DataSourceFilesGenerator
-import com.mitteloupe.cag.core.generation.bulk.DataSourceModulesGenerator
-import com.mitteloupe.cag.core.generation.bulk.FeatureFilesGenerator
-import com.mitteloupe.cag.core.generation.bulk.ProjectTemplateFilesGenerator
-import com.mitteloupe.cag.core.generation.bulk.ViewModelFilesGenerator
-import com.mitteloupe.cag.core.generation.filesystem.FileCreator
-import com.mitteloupe.cag.core.generation.gradle.GradleFileCreator
-import com.mitteloupe.cag.core.generation.gradle.GradlePropertiesFileCreator
-import com.mitteloupe.cag.core.generation.gradle.GradleWrapperCreator
-import com.mitteloupe.cag.core.generation.layer.DataLayerContentGenerator
-import com.mitteloupe.cag.core.generation.layer.DataSourceImplementationCreator
-import com.mitteloupe.cag.core.generation.layer.DataSourceInterfaceCreator
-import com.mitteloupe.cag.core.generation.layer.DomainLayerContentGenerator
-import com.mitteloupe.cag.core.generation.layer.PresentationLayerContentGenerator
-import com.mitteloupe.cag.core.generation.layer.UiLayerContentGenerator
-import com.mitteloupe.cag.core.generation.versioncatalog.VersionCatalogUpdater
 import com.mitteloupe.cag.core.request.GenerateArchitectureRequest
 import com.mitteloupe.cag.core.request.GenerateFeatureRequest
 import com.mitteloupe.cag.core.request.GenerateUseCaseRequest
@@ -876,77 +851,5 @@ dependencies {
         assertEquals(expectedContent, uiGradle)
     }
 
-    private fun produceGenerator(): Generator {
-        val fileCreator = FileCreator(FakeFileSystemBridge())
-        val directoryFinder = DirectoryFinder()
-        val kotlinFileCreator = KotlinFileCreator(fileCreator)
-        val gradleFileCreator = GradleFileCreator(fileCreator)
-        val gradleWrapperCreator = GradleWrapperCreator(fileCreator)
-        val buildSrcContentCreator = BuildSrcContentCreator(fileCreator)
-        val gradlePropertiesFileCreator = GradlePropertiesFileCreator(fileCreator)
-        val catalogUpdater = VersionCatalogUpdater(fileCreator)
-        val architectureModulesContentGenerator = ArchitectureModulesContentGenerator(gradleFileCreator, catalogUpdater)
-        val coroutineModuleContentGenerator = CoroutineModuleContentGenerator(gradleFileCreator, catalogUpdater)
-        val uiLayerContentGenerator = UiLayerContentGenerator(kotlinFileCreator)
-        val dataLayerContentGenerator = DataLayerContentGenerator(kotlinFileCreator)
-        val dataSourceDependencyInjectionModuleCreator = DataSourceDependencyInjectionModuleCreator(fileCreator)
-        val dataSourceInterfaceCreator = DataSourceInterfaceCreator(fileCreator)
-        val dataSourceImplementationCreator = DataSourceImplementationCreator(fileCreator)
-        val settingsFileUpdater = SettingsFileUpdater(fileCreator)
-        val domainLayerContentGenerator = DomainLayerContentGenerator(kotlinFileCreator)
-        val presentationLayerContentGenerator = PresentationLayerContentGenerator(kotlinFileCreator, fileCreator)
-        val configurationFileCreator = ConfigurationFileCreator(fileCreator)
-        val appModuleContentGenerator = AppModuleContentGenerator(fileCreator, directoryFinder)
-        val dataSourceModulesGenerator =
-            DataSourceModulesGenerator(
-                catalogUpdater,
-                gradleFileCreator,
-                settingsFileUpdater
-            )
-        val architectureFilesGenerator =
-            ArchitectureFilesGenerator(
-                coroutineModuleContentGenerator,
-                architectureModulesContentGenerator,
-                settingsFileUpdater,
-                buildSrcContentCreator,
-                configurationFileCreator
-            )
-        val featureFilesGenerator =
-            FeatureFilesGenerator(
-                catalogUpdater,
-                gradleFileCreator,
-                domainLayerContentGenerator,
-                presentationLayerContentGenerator,
-                dataLayerContentGenerator,
-                uiLayerContentGenerator,
-                settingsFileUpdater,
-                configurationFileCreator,
-                appModuleContentGenerator
-            )
-        return Generator(
-            domainLayerContentGenerator,
-            featureFilesGenerator,
-            dataSourceModulesGenerator,
-            DataSourceFilesGenerator(
-                dataSourceInterfaceCreator,
-                dataSourceImplementationCreator,
-                dataSourceDependencyInjectionModuleCreator
-            ),
-            architectureFilesGenerator,
-            ProjectTemplateFilesGenerator(
-                catalogUpdater,
-                settingsFileUpdater,
-                configurationFileCreator,
-                gradleFileCreator,
-                gradlePropertiesFileCreator,
-                gradleWrapperCreator,
-                appModuleContentGenerator,
-                buildSrcContentCreator,
-                dataSourceModulesGenerator,
-                architectureFilesGenerator,
-                featureFilesGenerator
-            ),
-            ViewModelFilesGenerator(presentationLayerContentGenerator)
-        )
-    }
+    private fun produceGenerator(): Generator = GeneratorFactory(FakeFileSystemBridge()).create()
 }
